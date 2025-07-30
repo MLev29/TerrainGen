4bb1e4c4add601894fcebc165c726d0d5918ab15
MLev29 <ml.290620056@gmail.com>
2025-07-30 17:11:41 -0400

    Clean up buffer class
    Remove unused code and delete buffer in destructor

--- src/utility/Buffer.cpp
+++ src/utility/Buffer.cpp
@@ -2,39 +2,6 @@
 
 #include "glad/glad.h"
 
-//src::Buffer::Buffer(void)
-//	: m_buffer(0)
-//{
-//	glCreateBuffers(1, &m_buffer);
-//}
-//
-//void src::Buffer::Init(void)
-//{
-//	if (!m_buffer)
-//		glCreateBuffers(1, &m_buffer);
-//}
-//
-//unsigned int src::Buffer::GetBufferID(void) const noexcept
-//{
-//	return m_buffer;
-//}
-//
-//void src::Buffer::SetData(void* data, size_t size)
-//{
-//	glNamedBufferData(m_buffer, size, data, GL_STATIC_DRAW);
-//}
-//
-//void src::Buffer::SetData(void* data, size_t size, unsigned int offset)
-//{
-//	glNamedBufferSubData(m_buffer, offset, size, data);
-//}
-//
-//void src::Buffer::DeleteData(void)
-//{
-//	glDeleteBuffers(1, &m_buffer);
-//	m_buffer = 0;
-//}
-
 src::Buffer::Buffer(void)
 {
 	glCreateBuffers(1, &m_buffer);
@@ -42,6 +9,7 @@ src::Buffer::Buffer(void)
 
 src::Buffer::~Buffer(void)
 {
+	glDeleteBuffers(1, &m_buffer);
 	m_buffer = 0;
 }
 
